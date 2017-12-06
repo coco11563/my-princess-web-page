5aadf60f932437f73586c809ae6175568e82f2b9
coco11563 <coco11563@yeah.net>
2017-12-07 00:30:21 +0800

    Initial commit

---
+++ src/main/java/sha0w/pub/myprincesswebpage/MyPrincessWebPageApplication.java
@@ -0,0 +1,12 @@
+package sha0w.pub.myprincesswebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPrincessWebPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPrincessWebPageApplication.class, args);
+	}
+}
